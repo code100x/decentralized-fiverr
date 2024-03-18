c72ee18c7f75a3752b14f06b8515b4b9eaaeeeac
Dewansh Chaudhari <codewansh@Dewanshs-MacBook-Pro.local>
2024-03-19 02:35:16 +0530

    turborepo storybook init

---
+++ apps/storybook/.storybook/main.js
@@ -0,0 +1,42 @@
+import { dirname, join, resolve } from "path";
+
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
+}
+
+const config = {
+  stories: ["../stories/*.stories.tsx", "../stories/**/*.stories.tsx"],
+  addons: [
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-docs"),
+  ],
+  framework: {
+    name: getAbsolutePath("@storybook/react-vite"),
+    options: {},
+  },
+
+  core: {},
+
+  async viteFinal(config, { configType }) {
+    // customize the Vite config here
+    return {
+      ...config,
+      define: { "process.env": {} },
+      resolve: {
+        alias: [
+          {
+            find: "ui",
+            replacement: resolve(__dirname, "../../../packages/ui/"),
+          },
+        ],
+      },
+    };
+  },
+
+  docs: {
+    autodocs: true,
+  },
+};
+
+export default config;
