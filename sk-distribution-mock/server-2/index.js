4a1276c72fe135abac03f84c89ad804dba6c04b2
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-05-13 02:13:59 +0530

    secret-key distribution among different servers

---
+++ sk-distribution-mock/server-2/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+// Vernalibity: Only allow request from our main backend
+app.use(cors());
+
+app.get("/share", (req, res) => {
+  console.log(req.headers);
+  const share = process.env.SHARE;
+  if (!share) {
+    return res.status(404).json({ msg: "Share not found." });
+  }
+  res.status(200).json({ share });
+});
+
+app.listen(7070);
