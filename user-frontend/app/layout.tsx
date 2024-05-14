e25381d57cb48e7d0bad621063c26b523d73a545
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-05-15 00:14:27 +0530

    sonner notifications

--- user-frontend/app/layout.tsx
+++ user-frontend/app/layout.tsx
@@ -1,7 +1,7 @@
-
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Toaster richColors />
+        {children}
+      </body>
     </html>
   );
 }
